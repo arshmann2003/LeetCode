b832039e4c1e7cd9c1a23e2cc331b6f7c303002b
Arsh Mann <asm32@sfu.ca>
2023-07-01 13:56:25 -0700

    Easy: RomanToInt

---
+++ Easy/RomanToInt.java
@@ -0,0 +1,28 @@
+package Easy;
+import java.util.HashMap;
+
+class RomanToInt {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int num = map.get(s.charAt(i));
+            if (i < n - 1 && num < map.get(s.charAt(i + 1))) {
+                ans -= num;
+            } else {
+                ans += num;
+            }
+        }
+        return ans;
+    }
+}
